14ec730b297abf0702e52964d233ae356500d441
Sizhe Fan <132420031+paoxiaomooo@users.noreply.github.com>
2024-07-11 17:54:44 +0800

    Update pxm万能爬虫.py

--- pxm万能爬虫.py
+++ pxm万能爬虫.py
@@ -1,20 +1,19 @@
 import yt_dlp
 
-
-def list_formats(url):
+def list_formats(url, cookies_file=''):
     ydl_opts = {
         'listformats': True,
-        'cookiefile': cookies_file
+        #'cookiefile': cookies_file
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-def download_video(url, format_code):
+def download_video(url, format_code, cookies_file=''):
     ydl_opts = {
         'format': format_code,
         'outtmpl': '%(title)s.%(ext)s',
         'merge_output_format': 'mp4',
-        'cookiefile': cookies_file,
+        #'cookiefile': cookies_file,
         'postprocessors': [{
             'key': 'FFmpegVideoConvertor',
             'preferedformat': 'mp4',
@@ -22,7 +21,9 @@ def download_video(url, format_code):
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
-video_url = input("请输入视频网址:  ")
+
+cookies_file = "cookies.txt"
+video_url = input("请输入视频网址: ")
 list_formats(video_url)
 format_code = input("请输入你选择的格式代码: ")
 
